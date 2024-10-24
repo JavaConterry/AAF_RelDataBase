472c4bdb389858797156d0198ff8f0b59dec61c0
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-10-24 22:04:03 +0300

    Reworked parser for create

--- database/inputparser.py
+++ database/inputparser.py
@@ -17,6 +17,28 @@ class InputParser:
         }
         self.registered_words = ['CREATE', 'INDEXED', 'INSERT', 'INTO', 'SELECT', 'FROM', 'WHERE']
 
+    def _count_brackets(self, user_command):
+        count_for_brackets = {'(': user_command.count('('), ')': user_command.count(')')}
+        bracket_open = count_for_brackets['(']
+        bracket_close = count_for_brackets[')']
+        if bracket_open == 0:
+            return self.exception(user_command, 'There is no "(" in CREATE command')
+        elif bracket_open != 1:
+            return self.exception(user_command, f'There is {bracket_open} "(" in CREATE command')
+        if bracket_close == 0:
+            return self.exception(user_command, 'There is no ")" in CREATE command')
+        elif bracket_close != 1:
+            return self.exception(user_command, f'There is {bracket_close} ")" in CREATE command')
+
+    def _enter_command_check(self, enter_command, second_argument=None):
+        if len(enter_command) == 1:
+            return self.exception(enter_command, f'There is no table name in {enter_command[0]} command')
+        if len(enter_command) >= 3:
+            if second_argument is None:
+                return self.exception(enter_command, f'Wrong {enter_command[0]} command syntax')
+            elif enter_command[1] != second_argument:
+                return self.exception(enter_command, f'Wrong {enter_command[0]} command syntax')
+
     def help(self, user_command):
         print('CREATE table_name (column_name [INDEXED] [, ...]);')
         print('INSERT [INTO] table_name ("value" [, ...]);')
@@ -24,32 +46,30 @@ class InputParser:
         return user_command
 
     def create(self, user_command):
-        if len(user_command.split('(')) != 2:
-            return self.exception(user_command, 'There is no () or too many () in CREATE command')
+        check_brackets = self._count_brackets(user_command)
 
-        user_command_str = re.sub(r'[(),]', '', user_command)
-        user_command = user_command_str.split()
+        if check_brackets is not None:
+            return check_brackets
 
-        request = ['CREATE']
+        user_command_str = user_command
 
-        table_name = user_command[1]
-        if table_name in self.registered_words:
-            return self.exception(user_command_str, "Table name can't be reserved word")
-        request.append(table_name)
-        request.append([])
+        command = user_command[:-1].split('(')
+        enter_command, main_command = command[0].split(), command[1].split(',')
 
-        prev_word = None
-        for word in user_command[2:]:
-            if prev_word is not None:
-                if prev_word.lower() == 'indexed' and word.lower() == 'indexed':
-                    return self.exception(user_command_str)
-                elif word.lower() == 'indexed':
-                    request[-1].append([prev_word, 'INDEXED'])
-                elif prev_word.lower() != 'indexed':
-                    request[-1].append([prev_word])
-            prev_word = word
-        request[-1].append([prev_word])
-        return request  # -> CREATE table_name [[colname, INDEXED], [colname], [...]]
+        self._enter_command_check(enter_command)
+
+        for word_index in range(len(main_command)):
+            word_list = main_command[word_index].split()
+            main_command[word_index] = main_command[word_index].strip()
+            if len(word_list) > 2:
+                return self.exception(user_command_str, f'Wrong {enter_command[0]} command syntax, no spaces in table column name ({command[1]})')
+            if len(word_list) == 2:
+                if word_list[1].lower() != 'indexed':
+                    return self.exception(user_command_str, f'Wrong {enter_command[0]} command syntax, no spaces in table column name ({command[1]})')
+            if word_list[0].lower() in self.registered_words:
+                return self.exception(user_command_str, "Column name can't be reserved word")
+
+        return [enter_command, main_command]
 
     def insert(self, user_command):
         if len(user_command.split('(')) != 2:
@@ -112,20 +132,30 @@ class InputParser:
         return request # -> SELECT FROM table_name WHERE [input1, input2, ...]
 
     def exception(self, user_command, explain='No such command'):
-        text = f"Command {user_command} is not supported!\nExplaining: {explain}"
+        text = f"[!] Command {user_command} is not supported!\n[?] Explaining: {explain}"
         print(text)
         return text
 
     def parse_input(self, command):
-        command = re.sub(r'[^\w\s(),=><]', '', command)     # remove non-words
-        # print(command)
+        # command = re.sub(r"""[^\w\s(),=><'";]""", '', command)     # remove non-words ^\w\s(),=><
+        command = command.split(";")[0]
+        command = (re.sub(r"""[^\w\s(),=><'"]""", '', command)).strip()
+        print(command)
         command_to_parse = self.dict.get(command.split()[0].lower(), self.exception)
         return command_to_parse(command)
 
 
 if __name__ == "__main__":
     parser = InputParser()
-    print(parser.parse_input('INSERT table_name (value1, value2);'))
-    parser.parse_input('Hello from hell kids;')
-    print(parser.parse_input('CREATE shit (name is shit, kiss INDEXED my ass);'))
-    print(parser.parse_input('SELECT FROM shit WHERE name = shit AND age < 10 OR name = Heavy;'))
+    print(parser.parse_input('CREATE students \n (id INDEXED, name, age, sex);'))
+    # print(parser.parse_input('  \n  \t \r   &   CREATE students (id INDEXED, & name, age, sex) & ; & \n  \t \r      '))
+    # (parser.parse_input('  \n  \t \r       CREATE students (id INDEXED, name is shit, age, sex)                  ;       sex              '))
+    # (parser.parse_input('CREATE students (id INDEXED, "name is shit", age, sex);'))
+    # (parser.parse_input("""CREATE students ('name "dad"');"""))
+    # (parser.parse_input("CREATE students (id INDEXED, 'name is shit', age, sex);"))
+    # (parser.parse_input('CREATE students (id INDEXED, INDEXED name is shit, age, sex);'))
+    # (parser.parse_input('CREATE students (id INDEXED, name is shit, age, sex;'))
+    # (parser.parse_input('CREATE students id INDEXED, name is shit, age, sex;'))
+    print(parser.parse_input('INSERT students (Dave, 18, male);'))
+    print(parser.parse_input('SELECT FROM students WHERE name = Dave AND age < 10 OR name = Heavy;'))
+    parser.parse_input('Hello from hell, students;')
