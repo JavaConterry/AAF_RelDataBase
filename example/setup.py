9a68ffec841fb44792f3bebc477d4950f0ac8bba
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-11-17 20:06:12 +0200

    Test...

---
+++ example/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='e',
+    packages=find_packages(include=['e']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me'
+)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='database',
+    packages=find_packages(include=['database']),
+    version='0.1.0',
+    description='Database',
+    author='KlymentievMaksym Dadmon, JavaConterry Conterry '
+)
