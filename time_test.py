d60b074b7658206d5190667ea293580883552b64
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-12-18 13:25:21 +0200

    Fixed 'Decashing' :3

--- time_test.py
+++ time_test.py
@@ -27,10 +27,10 @@ class Core():
         # Welcome message
         print("Welcome to the Database!")
 
-        main_input = input()
+        main_input = input(">>> ")
         while main_input != 'exit' and main_input != '':
             while ';' not in main_input:
-                ask = input()
+                ask = input(">>> ")
                 main_input += ' ' + ask
                 if ask == "" or ask == 'exit':
                     main_input = 'exit'
@@ -41,10 +41,12 @@ class Core():
             # command pipeline
             command = parser.parse_input(main_input)
             if(command[0][0] == 'SELECT'):
-                start_time = time.time()
-                response = db.do(command)
-                print('Execution Time: ', time.time() - start_time)
-                # print('Result size: ', response.column_trees[0].total_data_length())
+                result = []
+                for tries in range(3):
+                    start_time = time.time()
+                    response = db.do(command)
+                    result.append(time.time() - start_time)
+                print('Execution Time: ', sum(result)/len(result))
             else:
                 response = db.do(command)
 
@@ -52,7 +54,7 @@ class Core():
             # visualised_response.visualize()
 
 
-            main_input = input()
+            main_input = input(">>> ")
 
 
 if __name__ == "__main__":
